05f94b88ee7a5f731c08b3cca497bec8901d9642
Hao Man <101798822+manhao9843@users.noreply.github.com>
2022-07-29 15:14:31 +0800

    Loading data

---
+++ DataLoading.py
@@ -0,0 +1,31 @@
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+class MyDataset(Dataset):
+    def __init__(self, encoded_list, labels):
+        self.features = encoded_list
+        self.labels = labels
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self,idx):
+        data = self.features[idx]
+        label = self.labels[idx]
+        
+        return data, label
+
+def collate_batch(batch):
+    label_list, text_list, = [], []
+   
+    for (_text,_label) in batch:
+        label_list.append(_label)
+        text_list.append(_text)
+   
+    label_list = torch.tensor(label_list, dtype=torch.int64)
+    text_list = pad_sequence(text_list, batch_first=True, padding_value=0)
+   
+    return text_list,label_list
+
