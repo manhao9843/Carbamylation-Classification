b5ff24a9227d862c9006d070a95a494316c5bf69
Hao Man <101798822+manhao9843@users.noreply.github.com>
2022-08-29 13:48:55 +0800

    Attention Block

--- AttentionBlocks.py
+++ AttentionBlocks.py
@@ -1,6 +1,26 @@
 import torch
 from torch import nn
 
+# codes from github.com/d2l-ai/d2l-zh
+def sequence_mask(X, valid_len, value=0):
+    maxlen = X.size(1)
+    mask = torch.arange((maxlen), dtype=torch.float32)[None, :] < valid_len[:, None]
+    X[~mask] = value
+    return X
+
+def masked_softmax(X, valid_lens):
+    if valid_lens is None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+        if valid_lens.dim() == 1:
+            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+        else:
+            valid_lens = valid_lens.reshape(-1)
+        X = sequence_mask(
+                X.reshape(-1, shape[-1]), valid_lens, value = -1e6)
+    return nn.functional.softmax(X.reshape(shape), dim=-1)
+
 class AdditiveAttention(nn.Module):
     def __init__(self, key_size, query_size, num_hiddens, dropout, **kwargs):
         super(AdditiveAttention, self).__init__(**kwargs)
@@ -10,42 +30,21 @@ class AdditiveAttention(nn.Module):
         self.W_v = nn.Linear(num_hiddens, 1, bias=False)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, queries, keys, values):
-        # queries: [batch_size, num_queries, h]
-        # keys: [batch_size, num_keys, h]
+    def forward(self, queries, keys, values, valid_lens):
+        # queries: [batch_size, num_queries, n_hiddens]
+        # keys: [batch_size, num_keys, n_hiddens]
         queries, keys = self.W_q(queries), self.W_k(keys)
     
-        # queries: [batch_size, num_queries, 1.      , h]
-        # keys:    [batch_size, 1.         , num_keys, h]
-        # features:[batch_size, num_queries, num_keys, h]
+        # queries: [batch_size, num_queries, 1.      , n_hiddens]
+        # keys:    [batch_size, 1.         , num_keys, n_hiddens]
+        # features:[batch_size, num_queries, num_keys, n_hiddens]
         features = queries.unsqueeze(2) + keys.unsqueeze(1)
         features = torch.tanh(features)
 
         # scores: [batch_size, num_queries, num_keys]
-        # values: [batch_size, num_keys,    len_of_value]
         scores = self.W_v(features).squeeze(-1)
-        self.attention_weights = nn.functional.softmax(scores,dim=-1)
-        # [batch_size, num_queries, len_of_value]
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        
+        # values: [batch_size, num_keys,    len_of_value]
+        # output: [batch_size, num_queries, len_of_value]
         return torch.bmm(self.dropout(self.attention_weights), values)
-
-class DotProductAttention(nn.Module):
-    def __init__(self, key_size, query_size, num_hiddens, dropout, **kwargs):
-        super(DotProductAttention, self).__init__(**kwargs)
-
-        self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
-        self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
-        self.W_v = nn.Linear(num_hiddens, 1, bias=False)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, queries, keys, values):
-        # queries: [batch_size, num_queries, h]
-        # keys: [batch_size, num_keys, h]
-        queries, keys = self.W_q(queries), self.W_k(keys)
-
-        # scores: [batch_size, num_queries, num_keys]
-        scores = torch.bmm(queries, torch.transpose(keys, 1, 2))
-        self.attention_weights = nn.functional.softmax(scores,dim=-1)
-
-        # [batch_size, num_queries, len_of_value]
-        return torch.bmm(self.dropout(self.attention_weights), values)
-
