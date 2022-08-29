b475f496bbf6d4242c7400c3c2eaf3b4126ce20d
Hao Man <101798822+manhao9843@users.noreply.github.com>
2022-08-29 14:57:57 +0800

    Implementation of Focal Loss with Label Smoothing

--- Loss.py
+++ Loss.py
@@ -3,29 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class FocalLoss(nn.Module):
-    def __init__(self, device, gamma=2.0, alpha_1=0.25):
-        super(FocalLoss, self).__init__()
-        self.device = device
-        self.gamma = gamma
-        self.alpha = torch.tensor([1-alpha_1, alpha_1])
-    
-    def forward(self, inputs, targets):
-        
-        # one-hot encode the labels
-        idx = targets.view(-1,1)
-        one_hot_key = torch.nn.functional.one_hot(idx, 2).squeeze(1).to(self.device)
-        
-        # softmax
-        probs = nn.Softmax(dim=1)(inputs.float()).to(self.device)
-        
-        # compute loss
-        loss = -one_hot_key * torch.pow((1-probs), 2) * probs.log() * (self.alpha.to(self.device))
-        loss = loss.sum()
-        
-        return loss
-
-# from Github https://github.com/Kageshimasu/focal-loss-with-smoothing
+# original code from Github https://github.com/Kageshimasu/focal-loss-with-smoothing
 class FocalLossWithLabelSmoothing(nn.Module):
     def __init__(
             self,
